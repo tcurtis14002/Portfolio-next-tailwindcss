be73eecdc6f70bf27137a6c547ce1634e75e3b23
Tyler <willdev0110@gmail.com>
2023-04-06 16:12:38 -0600

    add skill, and skills component

---
+++ components/Skills.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Skill from "./Skill";
+
+function Skills() {
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+			className='relative mx-auto flex h-screen max-w-[2000px] flex-col items-center justify-evenly px-10 text-center md:flex-row md:text-left xl:flex-row '>
+			<h3 className='absolute top-24 min-h-screen text-2xl uppercase tracking-[20px] text-gray-500 xl:space-y-0'>
+				Skills
+			</h3>
+
+			<h3 className='absolute top-36 text-sm uppercase tracking-[3px] text-gray-500'>
+				Hover for current proficiency
+			</h3>
+			<div className='grid grid-cols-4 gap-5'>
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+			</div>
+		</motion.div>
+	);
+}
+
+export default Skills;
