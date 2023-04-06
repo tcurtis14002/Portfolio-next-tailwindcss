4a2aa6001ed7607940f5059a25448d5de4365b6f
Tyler <willdev0110@gmail.com>
2023-04-06 16:11:50 -0600

    finish about component

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,8 @@ function About() {
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className='relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-evenly px-10 md:flex-row md:text-left'>
+			viewport={{ once: true }}
+			className='relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-evenly px-10 text-center md:flex-row md:text-left'>
 			<h3 className='absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500'>
 				About
 			</h3>
@@ -28,14 +29,13 @@ function About() {
 				<Image
 					height={500}
 					width={500}
-					viewport={{ once: true }}
 					transition={{ duration: 1.2 }}
 					src={aboutPic}
 					className='md:h-95 md: -mb-20 h-60 w-60 flex-shrink-0 rounded-full object-cover md:mb-0 md:w-64 md:rounded-lg xl:h-[600px] xl:w-[500px]'
 				/>
 			</motion.div>
 
-			<div className='space-y-10 px-0 md:px-10'>
+			<div className='max-w-md space-y-10 px-0 md:px-10'>
 				<h4 className='text-4xl font-semibold'>
 					Here is a{" "}
 					<span className='underline decoration-[#F7AB0A]/50'>little</span>{" "}
