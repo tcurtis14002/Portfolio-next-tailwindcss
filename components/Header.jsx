e35706679f44a182798277261af9f893b8ff774f
Tyler <willdev0110@gmail.com>
2023-04-06 17:22:14 -0600

    fix header syntax

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 function Header() {
 	return (
-		<header className='sticky top-0 p-5 flex items-start justify-between max-w-6xl mx-auto z-20 xl:items-center '>
+		<header className='sticky top-0 z-20 mx-auto flex max-w-6xl items-start justify-between p-5 xl:items-center '>
 			{/* Social Header */}
 			<motion.div
 				initial={{
@@ -39,32 +40,33 @@ function Header() {
 				/>
 			</motion.div>
 
-			{/* Email Header */}
-			<motion.div
-				initial={{
-					x: 500,
-					opacity: 0,
-					scale: 0.5,
-				}}
-				animate={{
-					x: 0,
-					opacity: 1,
-					scale: 1,
-				}}
-				transition={{
-					duration: 1.5,
-				}}
-				className='flex flex-row items-center text-gray-300 cursor-pointer'>
-				<SocialIcon
-					className='cursor-pointer'
-					network='email'
-					fgColor='gray'
-					bgColor='transparent'
-				/>
-				<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-					Get in Touch
-				</p>
-			</motion.div>
+			<Link href='#contact'>
+				<motion.div
+					initial={{
+						x: 500,
+						opacity: 0,
+						scale: 0.5,
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+					className='flex cursor-pointer flex-row items-center text-gray-300'>
+					<SocialIcon
+						className='cursor-pointer'
+						network='email'
+						fgColor='gray'
+						bgColor='transparent'
+					/>
+					<p className='hidden text-sm uppercase text-gray-400 md:inline-flex'>
+						Get in Touch
+					</p>
+				</motion.div>
+			</Link>
 		</header>
 	);
 }
