21f1431972ab279e514b9e4dba2ea8f0849f4b52
Tyler <willdev0110@gmail.com>
2023-04-10 23:31:17 -0600

    add INDEX SCHEMA

--- sanity/schemas/schema.ts
+++ sanity/schemas/schema.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = [experience, skills, socials, pageInfo]
+import experience from './experience'
+import pageInfo from './pageInfo'
+import projects from './projects'
+import skill from './skill'
+import social from './social'
+
+export const schemaTypes = [experience, pageInfo, projects, skill, social]
