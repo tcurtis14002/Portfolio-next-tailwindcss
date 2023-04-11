2af482bd0c3c5380c0d39445ce4016780acb3efe
Tyler <willdev0110@gmail.com>
2023-04-10 23:31:25 -0600

    add experience schema

--- sanity/schemas/experience.ts
+++ sanity/schemas/experience.ts
@@ -1,55 +1,42 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'pageInfo',
-  title: 'PageInfo',
+  name: 'experience',
+  title: 'Experience',
   type: 'document',
   fields: [
     defineField({
-      name: 'name',
-      title: 'Name',
+      name: 'jobTitle',
+      title: 'JobTitle',
       type: 'string',
     }),
     defineField({
-      name: 'role',
-      title: 'Role',
-      type: 'string',
-    }),
-    defineField({
-      name: 'heroImage',
-      title: 'Image',
+      name: 'companyImage',
+      title: 'CompanyImage',
       type: 'image',
       options: {
         hotspot: true,
       },
     }),
     defineField({
-      name: 'backgroundInformation',
-      title: 'BackgroundInformation',
+      name: 'company',
+      title: 'Company',
       type: 'string',
     }),
     defineField({
-      name: 'profilePic',
-      title: 'ProfilePic',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
+      name: 'dateStarted',
+      title: 'DateStarted',
+      type: 'date',
     }),
     defineField({
-      name: 'phoneNumber',
-      title: 'PhoneNumber',
-      type: 'string',
+      name: 'dateEnded',
+      title: 'DateEnded',
+      type: 'date',
     }),
     defineField({
-      name: 'email',
-      title: 'Email',
-      type: 'string',
-    }),
-    defineField({
-      name: 'address',
-      title: 'Address',
-      type: 'string',
+      name: 'isCurrentlyWorkingHere',
+      title: 'IsCurrentlyWorkingHere',
+      type: 'boolean',
     }),
   ],
 })
