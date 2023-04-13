e0e2560b237968331667b48a1377470f62437ff8
Tyler <willdev0110@gmail.com>
2023-04-13 12:59:23 -0600

    change schemas from ts to js

--- sanity/schemas/experience.js
+++ sanity/schemas/experience.js
@@ -1,44 +1,42 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
+export default {
   name: 'experience',
   title: 'Experience',
   type: 'document',
   fields: [
-    defineField({
+    {
       name: 'jobTitle',
       title: 'JobTitle',
       type: 'string',
-    }),
-    defineField({
+    },
+    {
       name: 'companyImage',
       title: 'CompanyImage',
       type: 'image',
       options: {
         hotspot: true,
       },
-    }),
-    defineField({
+    },
+    {
       name: 'company',
       title: 'Company',
       type: 'string',
-    }),
-    defineField({
+    },
+    {
       name: 'dateStarted',
       title: 'DateStarted',
       type: 'date',
-    }),
-    defineField({
+    },
+    {
       name: 'dateEnded',
       title: 'DateEnded',
       type: 'date',
-    }),
-    defineField({
+    },
+    {
       name: 'isCurrentlyWorkingHere',
       title: 'IsCurrentlyWorkingHere',
       type: 'boolean',
-    }),
-    defineField({
+    },
+    {
       name: 'technologies',
       title: 'Technologies',
       type: 'array',
@@ -48,8 +46,8 @@ export default defineType({
           to: {type: 'skill'},
         },
       ],
-    }),
-    defineField({
+    },
+    {
       name: 'points',
       title: 'Points',
       type: 'array',
@@ -58,6 +56,6 @@ export default defineType({
           type: 'string',
         },
       ],
-    }),
+    },
   ],
-})
+}
--- sanity/schemas/project.js
+++ sanity/schemas/project.js
@@ -1,17 +1,15 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
+export default {
   name: 'project',
   title: 'Project',
   type: 'document',
   fields: [
-    defineField({
+    {
       name: 'title',
       title: 'Title',
       description: 'Title of the Project',
       type: 'string',
-    }),
-    defineField({
+    },
+    {
       name: 'image',
       title: 'Image',
 
@@ -19,14 +17,14 @@ export default defineType({
       options: {
         hotspot: true,
       },
-    }),
-    defineField({
+    },
+    {
       name: 'summary',
       title: 'Summary',
 
       type: 'text',
-    }),
-    defineField({
+    },
+    {
       name: 'technologies',
       title: 'Technologies',
       type: 'array',
@@ -36,12 +34,12 @@ export default defineType({
           to: {type: 'skill'},
         },
       ],
-    }),
-    defineField({
+    },
+    {
       name: 'linkToBuild',
       title: 'LinkToBuild',
 
       type: 'url',
-    }),
+    },
   ],
-})
+}
