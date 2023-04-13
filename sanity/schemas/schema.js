e1006afe10f52f24b1fb96c8aa41bd3bbecbc7c6
Tyler <willdev0110@gmail.com>
2023-04-13 13:01:07 -0600

    fix project name for successful import

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -2,11 +2,8 @@ import {createSchema} from 'sanity'
 
 import experience from './experience'
 import pageInfo from './pageInfo'
-import projects from './projects'
+import project from './project'
 import skill from './skill'
 import social from './social'
 
-export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([experience, pageInfo, projects, skill, social]),
-})
+export const schemaTypes = [experience, pageInfo, project, skill, social]
