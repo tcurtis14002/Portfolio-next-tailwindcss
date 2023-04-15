b91139c5492d68d5bd7f3d1f9ae57c14571570b3
Tyler <willdev0110@gmail.com>
2023-04-15 10:40:25 -0600

    finish data file

---
+++ pages/experienceData.js
@@ -0,0 +1,53 @@
+export const data = {
+	0: {
+		company: "Army",
+		title: "Medic",
+		started: "October, 2012",
+		ended: "October, 2019",
+		summary: ["point1", "point2", "point3"],
+	},
+	1: {
+		company: "Largo Medical Center - ER, ICU, Cardiac",
+		title: "Medical Assistant",
+		started: "January, 2016",
+		ended: "September, 2019",
+		summary: ["point1", "point2", "point3"],
+	},
+
+	2: {
+		company: "European Culture Trip",
+		title: "Traveler",
+		started: "December, 2017",
+		ended: "Janurary, 2018",
+		summary: ["point1", "point2", "point3"],
+	},
+
+	3: {
+		company: "Spain and Central America Excursion",
+		title: "Traveler",
+		started: "March, 2018",
+		ended: "April, 2018",
+		summary: ["point1", "point2", "point3"],
+	},
+	4: {
+		company: "Moffitt Cancer Center - ER",
+		title: "Medical Assistant",
+		started: "September, 2019",
+		ended: "March, 2021",
+		summary: ["point1", "point2", "point3"],
+	},
+	5: {
+		company: "European Culture Trip",
+		title: "Traveler",
+		started: "November, 2019",
+		ended: "Janurary, 2020",
+		summary: ["point1", "point2", "point3"],
+	},
+	6: {
+		company: "Codecademy",
+		title: "Student - Full Stack Engineer",
+		started: "January, 2021",
+		ended: "current",
+		summary: ["point1", "point2", "point3"],
+	},
+};
