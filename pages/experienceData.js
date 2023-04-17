e1cd1f9b61da60f5fab44390112917e34591009f
Tyler <willdev0110@gmail.com>
2023-04-17 10:41:20 -0600

    update experience data

--- pages/experienceData.js
+++ pages/experienceData.js
@@ -3,15 +3,18 @@ export const army = {
 	title: "Medic",
 	started: "October, 2012",
 	ended: "October, 2019",
-	summary: ["point1", "point2", "point3"],
+	summary: ["army1", "army2", ],
 	src: "/Army.jpg",
+	key: 1,
 };
 export const largo = {
 	company: "Largo Medical Center - ER, ICU, Cardiac",
 	title: "Medical Assistant",
 	started: "January, 2016",
 	ended: "September, 2019",
-	summary: ["point1", "point2", "point3"],
+	summary: ["largo1", "largo2", ],
+	src: "/HCA-Healthcare-Logo.png",
+	key: 2,
 };
 
 export const eu17 = {
@@ -19,7 +22,9 @@ export const eu17 = {
 	title: "Traveler",
 	started: "December, 2017",
 	ended: "Janurary, 2018",
-	summary: ["point1", "point2", "point3"],
+	summary: ["eu17 1", "eu17 2", ],
+	src: "/eu_17.jpg",
+	key: 3,
 };
 
 export const spain = {
@@ -27,26 +32,34 @@ export const spain = {
 	title: "Traveler",
 	started: "March, 2018",
 	ended: "April, 2018",
-	summary: ["point1", "point2", "point3"],
+	summary: ["sapin1", "spain2", ],
+	src: "/spain.jpg",
+	key: 4,
 };
 export const moffitt = {
 	company: "Moffitt Cancer Center - ER",
 	title: "Medical Assistant",
 	started: "September, 2019",
 	ended: "March, 2021",
-	summary: ["point1", "point2", "point3"],
+	summary: ["moffitt1", "moffitt2", ],
+	src: "/moffitt.webp",
+	key: 5,
 };
 export const eu19 = {
 	company: "European Culture Trip",
 	title: "Traveler",
 	started: "November, 2019",
 	ended: "Janurary, 2020",
-	summary: ["point1", "point2", "point3"],
+	summary: ["point1", "point2", ],
+	src: "/eu_19.jpg",
+	key: 6,
 };
 export const codecademy = {
 	company: "Codecademy",
 	title: "Student - Full Stack Engineer",
 	started: "January, 2021",
 	ended: "current",
-	summary: ["point1", "point2", "point3"],
+	summary: ["code1", "code2", ],
+	src: "/codecademy.jpeg",
+	key: 7,
 };
