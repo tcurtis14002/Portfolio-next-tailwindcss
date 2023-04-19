b545468b344471ab66f8357a070acd8fc19324ac
Tyler <willdev0110@gmail.com>
2023-04-18 18:05:41 -0600

    hard coded projects page. not using project component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,9 +2,10 @@ import { MotionConfig } from "framer-motion";
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { travel } from "../pages/projectsData";
+import { Project } from "./Project";
 
 function Projects() {
-	const projects = [1, 2, 3];
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -16,33 +17,32 @@ function Projects() {
 			</h3>
 
 			<div className='relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll '>
-				{projects.map((project, i) => {
-					<div className='flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44'>
-						<motion.img
-							initial={{
-								y: -100,
-								opacity: 0,
-							}}
-							transition={{ duration: 1.2 }}
-							whileInView={{ opacity: 1, y: 0 }}
-							viewport={{ once: true }}
-							src='/belgium_bg.jpg'
-							alt=''
-							className=''
-						/>
-						<div className='max-w-6xl space-y-10 px-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 md:px-10'>
-							<h4>Case Study {i + 1} of 3: Reddit Clone</h4>
-							<span className='underline decoration-[#F7AB0A]/50'></span>
-							<p className='text-center text-lg md:text-left'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Totam repellat vitae nobis fuga earum odit obcaecati quam
-								aut, veritatis, dolores dignissimos omnis sint! Molestias
-								earum iste necessitatibus illo, odit dicta magnam voluptas
-								magni ea delectus praesentium. Laborum fuga inventore cum!
-							</p>
-						</div>
-					</div>;
-				})}
+				<div className='flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44'>
+					<motion.img
+						initial={{
+							y: -100,
+							opacity: 0,
+						}}
+						transition={{ duration: 1.2 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						viewport={{ once: true }}
+						src='/coming_soon.jpg'
+						alt='image with text saying coming soon'
+						className='h-72 w-72 rounded-sm '
+					/>
+					<div className='max-w-6xl space-y-10 px-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 md:px-10'>
+						<h4>Travel Page</h4>
+						<span className='underline decoration-[#F7AB0A]/50'></span>
+						<p className='text-center text-lg md:text-left'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Totam repellat vitae nobis fuga earum odit obcaecati quam
+							aut, veritatis, dolores dignissimos omnis sint! Molestias
+							earum iste necessitatibus illo, odit dicta magnam voluptas
+							magni ea delectus praesentium. Laborum fuga inventore cum!
+						</p>
+					</div>
+				</div>
+				;
 			</div>
 
 			<div className='absolute left-0 top-[30%] h-[500px] w-full -skew-y-12 bg-[#F7AB0A]/10 blur-[2px]' />
