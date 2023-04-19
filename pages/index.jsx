7945f77e8052e786352524cf2a4123d029c1a908
Tyler <willdev0110@gmail.com>
2023-04-19 09:57:39 -0600

    delete unnedded ts and sanity files

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 	return (
 		<div className='scrollbar-width-5 z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
 			<Head>
-				<title>Tyler's Portfolio</title>
+				<title>Tyler&apos;s Portfolio</title>
 			</Head>
 
 			<Header />
--- portfolio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'd8dl6sqm',
-    dataset: 'production'
-  }
-})
