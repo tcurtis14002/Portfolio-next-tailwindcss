b3058628a3e0ff903f1415a8761fe6e0e3d348a0
Tyler <willdev0110@gmail.com>
2023-04-18 18:06:06 -0600

    finish skill component attached to data

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -2,7 +2,7 @@ import { MotionConfig } from "framer-motion";
 import React from "react";
 import { motion } from "framer-motion";
 
-function Skill({ directionLeft }) {
+function Skill({ directionLeft, src, proficiency }) {
 	return (
 		<div className='group relative flex cursor-pointer'>
 			<motion.img
@@ -10,11 +10,13 @@ function Skill({ directionLeft }) {
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
 				className='h-24 w-24 rounded-full border border-gray-500 object-cover filter transition duration-300 ease-in-out group-hover:grayscale md:h-28 md:w-28 xl:h-32 xl:w-32'
-				src='https://www.pngwing.com/en/free-png-azbzv'
+				src={src}
 			/>
 			<div className='absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32'>
 				<div className='flex h-full items-center justify-center'>
-					<p className='text-3xl font-bold text-black opacity-100'>100%</p>
+					<p className='text-3xl font-bold text-black opacity-100'>
+						{proficiency}
+					</p>
 				</div>
 			</div>
 		</div>
