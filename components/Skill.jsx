2057be3b666d7ff00cc8b41e7c39445a6e30b861
Tyler <willdev0110@gmail.com>
2023-04-21 10:00:01 -0600

    add title key to skill objs

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -6,14 +6,14 @@ function Skill({ directionLeft, src, proficiency }) {
 	return (
 		<div className='group relative flex cursor-pointer'>
 			<motion.img
-				initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+				initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
-				className='h-24 w-24 rounded-full border border-gray-500 object-cover filter transition duration-300 ease-in-out group-hover:grayscale md:h-28 md:w-28 xl:h-32 xl:w-32'
+				className='h-24 w-24 rounded-full border border-gray-500 object-cover filter transition duration-300 ease-in-out group-hover:grayscale sm:h-20 sm:w-20 md:h-28 md:w-28 xl:h-32 xl:w-32'
 				src={src}
 			/>
-			<div className='absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32'>
-				<div className='flex h-full items-center justify-center'>
+			<div className='absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 sm:h-20 sm:w-20 md:h-28 md:w-28 xl:h-32 xl:w-32'>
+				<div className='flex h-full w-full items-center justify-center'>
 					<p className='text-3xl font-bold text-black opacity-100'>
 						{proficiency}
 					</p>
--- data/skillData.js
+++ data/skillData.js
@@ -1,32 +1,40 @@
 export const react = {
+	title: "React",
 	src: "/react.png",
 	proficiency: "70%",
 };
 export const tailwindcss = {
+	title: "Tailwind CSS",
 	src: "/tailwind.png",
 	proficiency: "80%",
 };
 export const js = {
+	title: "JavaScript",
 	src: "/JavaScript.png",
 	proficiency: "75%",
 };
 export const ts = {
+	title: "TypeScript",
 	src: "/Typescript.png",
 	proficiency: "25%",
 };
 export const vsCode = {
+	title: "VS Code",
 	src: "/vs_code.png",
 	proficiency: "100%",
 };
 export const node = {
+	title: "Node JS",
 	src: "/node.webp",
 	proficiency: "75%",
 };
 export const next = {
+	title: "Next.js",
 	src: "/next.png",
 	proficiency: "80%",
 };
 export const sanity = {
+	title: "Sanity",
 	src: "/sanity.png",
 	proficiency: "25%",
 };
