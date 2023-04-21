0c11b97e30e63096213bbeff2afd9497a189b53a
Tyler <willdev0110@gmail.com>
2023-04-21 09:58:53 -0600

    change mobile opacity setting

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -5,13 +5,15 @@ function ExperienceCard({ work }) {
 	let i = 0;
 	return (
 		<article
-			className='flex w-[500px] flex-shrink-0 cursor-pointer snap-center flex-col items-center space-y-7 overflow-hidden rounded-lg bg-[#292929]
+			className='mt-16 flex w-[500px] flex-shrink-0 cursor-pointer snap-center flex-col items-center space-y-7 overflow-hidden rounded-lg bg-[#292929]
 			p-10
-			opacity-40
 			transition-opacity
 			duration-200
 			hover:opacity-100
-			md:w-[450px]'>
+			sm:w-[325px]
+			sm:opacity-100
+			md:w-[450px]
+			md:opacity-40'>
 			<motion.img
 				className='h-32 w-32 rounded-full object-cover object-center md:rounded-full xl:h-[200px] xl:w-[200px]'
 				src={work.src}
@@ -30,12 +32,20 @@ function ExperienceCard({ work }) {
 			<div className='px-0 md:px-10'>
 				<h4 className='text-4xl font-light'>{work.title}</h4>
 				<p className='mt-1 text-2xl font-bold'>{work.company}</p>
-				<p className='py-5 uppercase text-gray-300'>
-					Started: {work.started} Ended: {work.ended}
+				<p className='sm:py-1uppercase py-2 text-gray-300'>
+					Started: {work.started}
 				</p>
-				<ul className='ml-5 list-disc space-y-4 text-lg'>
+				<p className='sm:py-1uppercase py-2 text-gray-300'>
+					Ended: {work.ended}
+				</p>
+				<ul className='md:text-md ml-5 list-disc space-y-4 text-base text-lg sm:space-y-2 sm:text-sm'>
 					{work.summary.map((point) => (
-						<li key={i++}>{point}</li>
+						<li
+							key={i++}
+							className='text-just
+						px-1'>
+							{point}
+						</li>
 					))}
 				</ul>
 			</div>
