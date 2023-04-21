89d5576a57c49af7d163e12f9cd9a146f4a8f382
Tyler <willdev0110@gmail.com>
2023-04-21 09:59:29 -0600

    fix animation for mobile

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Skill from "./Skill";
+import { useState } from "react";
 import {
 	react,
 	tailwindcss,
@@ -12,20 +13,20 @@ import {
 } from "../data/skillData";
 
 function Skills() {
+	const [skill, setSkill] = useState(null);
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className='relative mx-auto flex h-screen max-w-[2000px] flex-col items-center justify-evenly px-10 text-center md:flex-row md:text-left xl:flex-row '>
-			<h3 className='absolute top-24 min-h-screen text-2xl uppercase tracking-[20px] text-gray-500 xl:space-y-0'>
+			className='relative mx-auto flex h-screen max-w-[2000px] flex-col items-center justify-evenly px-10 text-center sm:px-1 md:flex-row md:text-left xl:flex-row'>
+			<h3 className='absolute top-24 min-h-screen text-center text-2xl uppercase tracking-[20px] text-gray-500 xl:space-y-0'>
 				Skills
 			</h3>
-
-			<h3 className='absolute top-36 text-sm uppercase tracking-[3px] text-gray-500'>
+			<h3 className='absolute top-36 text-center text-sm uppercase tracking-[3px] text-gray-500'>
 				Hover for current proficiency
 			</h3>
-			<div className='grid grid-cols-4 gap-5'>
+			<div className='grid w-fit sm:grid-cols-2 sm:gap-2 sm:px-1 md:grid-cols-4 md:gap-5'>
 				<Skill
 					directionLeft={true}
 					src={react.src}
