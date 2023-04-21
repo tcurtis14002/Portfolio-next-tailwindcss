6f5e718d45bbe40a4dacc4455b62902018617477
Tyler <willdev0110@gmail.com>
2023-04-21 11:06:20 -0600

    ajust layout for mobile view

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -17,8 +17,8 @@ function WorkExperience() {
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className='relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-evenly px-10 text-center md:flex-row md:text-left '>
-			<h3 className='pb-auto absolute top-24 mx-auto text-2xl uppercase tracking-[20px] text-gray-500'>
+			className='align-center relative mx-auto flex h-screen max-w-7xl flex-col items-center justify-center px-10 text-center md:flex-row md:text-left'>
+			<h3 className='pb-auto align-center absolute top-24 mx-auto items-center text-center text-2xl uppercase tracking-[20px] text-gray-500'>
 				Experience
 			</h3>
 			<div className='flex w-full snap-x snap-mandatory space-x-5 overflow-x-scroll p-10 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
